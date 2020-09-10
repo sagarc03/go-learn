1ab16831034861d6cb3c72cf220dd60fcbe1ea36
Sagar Chavan <sagar.c.03@gmail.com>
2020-09-10 01:34:56 -0400

    add integers and iterations

---
+++ iteration/repeat.go
@@ -0,0 +1,10 @@
+package iteration
+
+// Repeat a character for given number of times.
+func Repeat(character string, times int) string {
+	var repeated string
+	for i := 0; i < times; i++ {
+		repeated = repeated + character
+	}
+	return repeated
+}
---
+++ iteration/repeat_test.go
@@ -0,0 +1,17 @@
+package iteration
+
+import "testing"
+
+func TestRepeat(t *testing.T) {
+	got := Repeat("a", 5)
+	want := "aaaaa"
+	if got != want {
+		t.Errorf("want %q but got %q", want, got)
+	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
+	}
+}
