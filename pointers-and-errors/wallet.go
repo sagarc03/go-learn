19affbde651570e57931a91e8f59d21a302ee33e
Sagar Chavan <sagar.c.03@gmail.com>
2020-09-26 02:06:40 -0400

    create bitcoin type and refactor test

--- pointers-and-errors/wallet.go
+++ pointers-and-errors/wallet.go
@@ -1,16 +1,19 @@
 package wallet
 
+// Bitcoin type
+type Bitcoin int
+
 // Wallet struct
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
 // Deposit the given amount in the account
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
 // Balance returns a balance in the wallet
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
